029d63ac0e25c47bded093a96ce1db50f9f16fc8
khrig <arnssn@gmail.com>
2014-11-25 22:17:13 +0100

    Updated tiles for background

--- Platformer/States/GameState.cs
+++ Platformer/States/GameState.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Gengine;
 using Gengine.Entities;
 using Gengine.State;
 using Gengine.Systems;
@@ -10,10 +11,11 @@ namespace Platformer.States {
     class GameState : State {
         private Texture2D environmentTexture;
 
-        private PlayerEntity player;
+        private readonly PlayerEntity player;
+        private readonly TileMap tileMap;
 
-        // Should not be here
-        private Renderer renderer;
+        // Should not be here probably
+        private readonly Renderer renderer;
 
         public GameState(SpriteBatch spriteBatch, Texture2D player, Texture2D environment) {
             this.environmentTexture = environment;
@@ -22,6 +24,8 @@ namespace Platformer.States {
                 new VisualComponent(player), 
                 new PositionComponent(new Vector2(100, 470)), 
                 new AnimationComponent(new Rectangle(0,0,32,32)));
+
+            tileMap = new TileMap(environment);
 
             renderer = new Renderer(spriteBatch);
         }
@@ -39,12 +43,7 @@ namespace Platformer.States {
         }
 
         public override bool Draw(SpriteBatch spriteBatch) {
-            Vector2 groundPos = new Vector2(0, 480);
-            for (int i = 1; i < 26; i++) {
-                spriteBatch.Draw(environmentTexture, groundPos, new Rectangle(0, 0, 32, 32), Color.White);
-                groundPos.X += 32;
-            }
-
+            renderer.Draw(tileMap.Tiles);
             renderer.Draw(new List<IRenderable> { player });
             return false;
         }
