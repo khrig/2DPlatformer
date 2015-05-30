306f407e401fc1509f61c27412ade80146426d22
Henrik Aronsson <arnssn@gmail.com>
2015-05-30 15:36:17 +0200

    Cache for commands

--- 2DPlatformer/PlatformerCommandFactory.cs
+++ 2DPlatformer/PlatformerCommandFactory.cs
@@ -1,9 +1,17 @@
-﻿using Gengine.Commands;
+﻿using System.Collections.Generic;
+using Gengine.Commands;
 
 namespace _2DPlatformer {
     public class PlatformerCommandFactory : ICommandFactory {
+        private readonly Dictionary<string, ICommand> _commands;
+        public PlatformerCommandFactory() {
+            _commands = new Dictionary<string, ICommand>();
+        }
+
         public ICommand CreateCommand(string name) {
-            return new Command(name);
+            if (!_commands.ContainsKey(name))
+                _commands.Add(name, new Command(name));
+            return _commands[name];
         }
     }
 }
